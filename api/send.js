1beba483e39447cd60acb5de6e05b8004045d0e6
Super-Meta77 <vodaguder@gmail.com>
2025-09-04 10:36:45 -0400

    fix error

--- api/send.js
+++ api/send.js
@@ -1,5 +1,11 @@
 const nodemailer = require('nodemailer');
 
+export const config = {
+  api: {
+    bodyParser: true // Enable body parsing
+  }
+};
+
 export default async function handler(req, res) {
   // Only allow POST method
   if (req.method !== 'POST') {
